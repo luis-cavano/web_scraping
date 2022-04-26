4d7b860a0ab5768e0e43e66edaf888eed7e3981f
LuisSevilha <luissevilhadev@gmail.com>
2022-04-26 18:01:50 -0300

    mostra pro sor pra ver oque precisa fazer

--- webscraping2.py
+++ webscraping2.py
@@ -1,24 +1,29 @@
 import requests
 import schedule
 import time
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 def job(): 
     url1 = requests.get('https://coinmarketcap.com/pt-br/currencies/bitcoin/')
     #print(url1.status_code)
-
     htmlBruto1 = url1.content
     site = BeautifulSoup(htmlBruto1, 'html.parser')
     tagPreco = site.find('div', attrs={'class':'priceValue smallerPrice'})
     #print(tagPreco)
     preco = tagPreco.find('span')
 
+    time = datetime.now()
+
+    salvar = [preco.text, str(time)]
+
     print()
-    print(preco.text)
+    print(salvar)
     print()
 
-schedule.every(10).seconds.do(job)
-
+schedule.every(2).minutes.do(job)
+#chedule.every(5).seconds.do(job)
+    
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    #time.sleep(0)
